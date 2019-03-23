cca6645bafd2582c37680308f0402264077ec3db
Alexey Arbuzov <aarbuzov@termt.com>
2019-03-23 13:58:36 +0300

    Import esp-idf SDK as git submodule

--- esp-idf-sys/setenv.sh
+++ esp-idf-sys/setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export PATH=/opt/gcc-xtensa-esp32-elf-1.22.0-80-g6c4433a-5.2.0/bin:$PATH
-export IDF_PATH=$HOME/git/esp-idf
+export IDF_PATH=$(pwd)/esp-idf
+export TARGET_DIR=target/xtensa-none-elf/release
