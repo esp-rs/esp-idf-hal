f6a8aeabb7dd59033df471cc42164038507dc748
Y. Sapir <yasapir@gmail.com>
2019-07-13 21:12:22 +0300

    Update target name

--- esp-idf-sys/setenv.sh
+++ esp-idf-sys/setenv.sh
@@ -2,4 +2,4 @@
 
 export PATH=/opt/gcc-xtensa-esp32-elf-1.22.0-80-g6c4433a-5.2.0/bin:$PATH
 export IDF_PATH=$(pwd)/esp-idf
-export TARGET_DIR=target/xtensa-none-elf/release
+export TARGET_DIR=target/xtensa-esp32-none-elf/release
