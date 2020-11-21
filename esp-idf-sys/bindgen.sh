1657bddfafe1c11ba53640872840dcde19581ab7
Ivan Markov <ivan.markov@gmail.com>
2020-11-21 14:16:34 +0200

    Upgrade to ESP-IDF V4.1 and include more APIs

--- esp-idf-sys/bindgen.sh
+++ esp-idf-sys/bindgen.sh
@@ -4,18 +4,24 @@ set -e
 
 . ./setenv.sh
 
-COMPS=$IDF_PATH/components
-: "${SYSROOT:=$(xtensa-esp32-elf-gcc --print-sysroot)}"
 TARGET=xtensa-esp32-none-elf
+XTENSA_GCC_TOOLCHAIN="$(dirname $(dirname $(which xtensa-esp32-elf-gcc)))"
+COMPS=$IDF_PATH/components
 
+#: "${SYSROOT:=$(xtensa-esp32-elf-gcc --print-sysroot)}"
+: "${SYSROOT:=$XTENSA_GCC_TOOLCHAIN/xtensa-esp32-elf}"
 : "${BINDGEN:=bindgen}"
-: "${LIBCLANG_PATH:=../llvm-project/llvm/build/lib}"
+#: "${LIBCLANG_PATH:=../llvm-project/llvm/build/lib}"
+: "${LIBCLANG_PATH:=$(dirname $(which clang))/../lib}"
+
 CLANG_FLAGS="\
     --sysroot=$SYSROOT \
     -I"$(pwd)" \
     -D__bindgen \
     --target=$TARGET \
     -x c"
+
+CLANG_FLAGS="${CLANG_FLAGS} -I$SYSROOT/sys-include"
 
 for INC in $(ls -d "$COMPS"/**/*/include); do
     CLANG_FLAGS="${CLANG_FLAGS} -I$INC"
--- esp-idf-sys/esp-idf-sys/src/bindings.h
+++ esp-idf-sys/esp-idf-sys/src/bindings.h
@@ -1,5 +1,31 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "esp_system.h"
+
+//#include "esp_crc.h"
+#include "esp_log.h"
+#include "esp_task.h"
+#include "esp_task_wdt.h"
+#include "esp_timer.h"
+#include "esp_spi_flash.h"
+#include "esp_int_wdt.h"
+#include "esp_interface.h"
+#include "esp_ipc.h"
+#include "esp_pm.h"
+
+#include "esp_event.h"
+
+#include "esp_netif.h"
+
+#include "esp_wifi.h"
+
+#include "esp_eth.h"
+#include "esp_eth_netif_glue.h"
+
+#include "esp_adc_cal.h"
+
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "esp32/rom/lldesc.h"
 #include "driver/adc.h"
 #include "driver/can.h"
 #include "driver/dac.h"
@@ -7,11 +33,29 @@
 #include "driver/i2c.h"
 #include "driver/i2s.h"
 #include "driver/ledc.h"
-#include "driver/rmt.h"
 #include "driver/mcpwm.h"
+#include "driver/pcnt.h"
+#include "driver/periph_ctrl.h"
+#include "driver/rmt.h"
+#include "driver/rtc_cntl.h"
+#include "driver/rtc_io.h"
+#include "driver/sdio_slave.h"
+#include "driver/sdmmc_defs.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdmmc_types.h"
+#include "driver/sdspi_host.h"
+#include "driver/sigmadelta.h"
 #include "driver/spi_common.h"
 #include "driver/spi_master.h"
 #include "driver/spi_slave.h"
 #include "driver/timer.h"
+#include "driver/touch_pad.h"
+#include "touch_sensor.h"
+#include "driver/touch_sensor_common.h"
 #include "driver/uart.h"
-#include <esp_clk.h>
+#include "driver/uart_select.h"
+
+#include "esp_core_dump.h"
+
+#include "esp_serial_slave_link/essl.h"
+#include "esp_serial_slave_link/essl_sdio.h"
