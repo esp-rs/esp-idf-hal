a514fc6450e03bfc792a727516b346fd9955df9c
Ivan Markov <ivan.markov@gmail.com>
2020-11-27 10:11:00 +0200

    Better error handling

--- esp-idf-sys/src/lib.rs
+++ esp-idf-sys/src/lib.rs
@@ -5,6 +5,7 @@
 
 use core::str;
 use core::slice;
+use core::fmt;
 
 pub mod c_types {
     pub enum c_void {}
@@ -45,10 +46,8 @@ impl Error {
         Error::check_and_return(error, ())
     }
 
-    pub fn check(error: esp_err_t) {
-        if error != 0 {
-            panic!("Got error code: {}", error);
-        }
+    pub fn panic(self: &Self) {
+        panic!("ESP ERROR: {}", self);
     }
 
     pub fn code(self: &Self) -> esp_err_t {
@@ -56,28 +55,22 @@ impl Error {
     }
 }
 
-// impl From<!> for Error {
-//     fn from(_: !) -> Self {
-//         loop {}
-//     }
-// }
-
-impl core::fmt::Display for Error {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        unsafe fn strlen(c_s: *const c_types::c_char) -> usize {
+            let mut len = 0;
+            while *c_s.offset(len) != 0 {
+                len += 1;
+            }
+        
+            len as usize
+        }
+        
         unsafe {
             let c_s = esp_err_to_name(self.code());
-            str::from_utf8_unchecked(slice::from_raw_parts(c_s as *const u8, strlen(c_s) + 1)).fmt(f)
+            str::from_utf8_unchecked(slice::from_raw_parts(c_s as *const u8, strlen(c_s))).fmt(f)
         }
     }
-}
-
-unsafe fn strlen(c_s: *const c_types::c_char) -> usize {
-    let mut len = 0;
-    while *c_s.offset(len) != 0 {
-        len += 1;
-    }
-
-    len as usize
 }
 
 #[macro_export]
@@ -88,9 +81,18 @@ macro_rules! esp {
 }
 
 #[macro_export]
-macro_rules! espr {
+macro_rules! esp_result {
     ($err:expr, $value:expr) => {{
-        Error::check_and_retrun($err as esp_err_t, value)
+        Error::check_and_return($err as esp_err_t, value)
+    }}
+}
+
+#[macro_export]
+macro_rules! esp_nofail {
+    ($err:expr) => {{
+        if let Some(error) = Error::from($err as esp_err_t) {
+            error.panic();
+        }
     }}
 }
 
