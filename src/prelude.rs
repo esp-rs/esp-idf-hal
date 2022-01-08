1dcb3705098803eb199509d605523f698f470f80
Andres Vahter <andres@vahter.me>
2022-01-08 12:43:55 +0200

    add CAN to peripherals.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,7 @@
 //! Instead of importing them one by one manually, the prelude contains the most
 //! commonly used imports that are used around application runtime management.
 //!
-//! This can be imported as use `esp_idf_hal::prelude::*`.
+//! This can be imported as `use esp_idf_hal::prelude::*`.
 
 pub use crate::peripherals::*;
 pub use crate::units::*;
