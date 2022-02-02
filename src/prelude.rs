efb9c88bace6630f4816822d0fcce46093215db9
ivmarkov <ivan.markov@gmail.com>
2022-02-02 15:35:05 +0200

    Revert "Re-export embedded-hal traits in prelude (#35)"
    This reverts commit a7b2a13513888dcc04315fc169d6d35b776465e3.

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,30 +6,5 @@
 //!
 //! This can be imported as `use esp_idf_hal::prelude::*`.
 
-pub use embedded_hal::{
-    adc::nb::{Channel as _, OneShot as _},
-    can::{Error as _, Frame as _},
-    delay::blocking::DelayUs as _,
-    digital::blocking::{
-        InputPin as _, OutputPin as _, StatefulOutputPin as _, ToggleableOutputPin as _,
-    },
-    i2c::{
-        blocking::{Read as _, Write as _, WriteRead as _},
-        Error as _,
-    },
-    serial::{
-        nb::{Read as _, Write as _},
-        Error as _,
-    },
-    spi::{
-        blocking::{
-            Read as _, Transactional as _, Transfer as _, TransferInplace as _, Write as _,
-            WriteIter as _,
-        },
-        Error as _,
-    },
-};
-pub use embedded_hal_0_2::prelude::*;
-
 pub use crate::peripherals::*;
 pub use crate::units::*;
