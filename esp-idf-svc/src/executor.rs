9cdd0a12f95480ba93e837fda1c1d3b2755e1ce4
ivmarkov <ivan.markov@gmail.com>
2022-04-27 16:04:18 +0300

    async executor traits

--- esp-idf-svc/src/executor.rs
+++ esp-idf-svc/src/executor.rs
@@ -1,17 +1,17 @@
 #[cfg(all(feature = "isr-async-executor", feature = "alloc"))]
 pub mod asyncs {
-    use core::ptr;
     use core::sync::atomic::{AtomicPtr, Ordering};
+    use core::{mem, ptr};
 
     extern crate alloc;
-    use alloc::sync::Arc;
+    use alloc::sync::{Arc, Weak};
 
     use embedded_svc::utils::asyncs::executor::*;
 
     use esp_idf_hal::interrupt;
 
-    pub type EspLocalExecutor<'a> = LocalExecutor<'a, CurrentTaskWait, SharedTaskHandle>;
-    pub type EspSendableExecutor<'a> = SendableExecutor<'a, CurrentTaskWait, SharedTaskHandle>;
+    pub type EspLocalExecutor<'a> = ISRExecutor<'a, TaskHandle, CurrentTaskWait, Local>;
+    pub type EspSendableExecutor<'a> = ISRExecutor<'a, TaskHandle, CurrentTaskWait, Sendable>;
 
     pub struct CurrentTaskWait;
 
@@ -21,50 +21,71 @@ pub mod asyncs {
         }
     }
 
-    pub struct SharedTaskHandle(Arc<AtomicPtr<esp_idf_sys::tskTaskControlBlock>>);
+    pub struct TaskHandle(Arc<AtomicPtr<esp_idf_sys::tskTaskControlBlock>>);
 
-    impl SharedTaskHandle {
+    impl TaskHandle {
         fn new() -> Self {
             Self(Arc::new(AtomicPtr::new(ptr::null_mut())))
         }
     }
 
-    impl Notify for SharedTaskHandle {
-        fn prerun(&self) {
-            self.0
-                .store(interrupt::task::current().unwrap(), Ordering::SeqCst);
-        }
+    impl Drop for TaskHandle {
+        fn drop(&mut self) {
+            let mut arc = mem::replace(&mut self.0, Arc::new(AtomicPtr::new(ptr::null_mut())));
 
-        fn notify(&self) {
-            let freertos_task = self.0.load(Ordering::SeqCst);
-
-            if !freertos_task.is_null() {
-                unsafe {
-                    interrupt::task::notify(freertos_task, 1);
+            // Busy loop until we can destroy the Arc - which means that nobody is actively holding a strong reference to it
+            // and thus trying to notify our FreeRtos task, which will likely be destroyed afterwards
+            loop {
+                match Arc::try_unwrap(arc) {
+                    Ok(_) => break,
+                    Err(a) => arc = a,
                 }
             }
         }
+    }
 
-        fn postrun(&self) {
-            self.0.store(ptr::null_mut(), Ordering::SeqCst);
+    impl NotifyFactory for TaskHandle {
+        type Notify = SharedTaskHandle;
+
+        fn notifier(&self) -> Self::Notify {
+            SharedTaskHandle(Arc::downgrade(&self.0))
         }
     }
 
-    impl Clone for SharedTaskHandle {
-        fn clone(&self) -> Self {
-            Self(self.0.clone())
+    impl RunContextFactory for TaskHandle {
+        fn prerun(&self) {
+            let current_task = interrupt::task::current().unwrap();
+            let stored_task = self.0.load(Ordering::SeqCst);
+
+            if stored_task.is_null() {
+                self.0.store(current_task, Ordering::SeqCst);
+            } else if stored_task != current_task {
+                panic!("Cannot call prerun() twice from two diffeent threads");
+            }
+        }
+    }
+
+    pub struct SharedTaskHandle(Weak<AtomicPtr<esp_idf_sys::tskTaskControlBlock>>);
+
+    impl Notify for SharedTaskHandle {
+        fn notify(&self) {
+            if let Some(notify) = self.0.upgrade() {
+                let freertos_task = notify.load(Ordering::SeqCst);
+
+                if !freertos_task.is_null() {
+                    unsafe {
+                        interrupt::task::notify(freertos_task, 1);
+                    }
+                }
+            }
         }
     }
 
     pub fn local<'a>(max_tasks: usize) -> EspLocalExecutor<'a> {
-        let task_handle = SharedTaskHandle::new();
-
-        LocalExecutor::new(max_tasks, CurrentTaskWait, task_handle)
+        ISRExecutor::<_, _, Local>::new(max_tasks, TaskHandle::new(), CurrentTaskWait)
     }
 
     pub fn sendable<'a>(max_tasks: usize) -> EspSendableExecutor<'a> {
-        let task_handle = SharedTaskHandle::new();
-
-        SendableExecutor::new(max_tasks, CurrentTaskWait, task_handle)
+        ISRExecutor::<_, _, Sendable>::new(max_tasks, TaskHandle::new(), CurrentTaskWait)
     }
 }
