ee4e12c04a0166468b7a4776154f8cdd0ab0ff3d
ivmarkov <ivan.markov@gmail.com>
2022-05-02 15:45:49 +0300

    Clippy

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() -> anyhow::Result<()> {
 
     // Will not be available when built with a CMake-first or a PIO-first build
     // We need to output these only when building the examples' binaries anyway
-    if let Some(args) = embuild::build::LinkArgs::try_from_env("ESP_IDF").ok() {
+    if let Ok(args) = embuild::build::LinkArgs::try_from_env("ESP_IDF") {
         args.output();
     }
 
