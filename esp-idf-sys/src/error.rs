e07cc633a9a9cef15d8fcad193be7ac923ec132d
Dominik Gschwind <dominik.gschwind99@gmail.com>
2022-08-19 02:06:10 +0200

    Track the caller when panicking on `EspError`
    Instructs Rust to print the source location of the caller instead of `EspError::panic`.

--- esp-idf-sys/src/error.rs
+++ esp-idf-sys/src/error.rs
@@ -40,6 +40,7 @@ impl EspError {
     }
 
     /// Panic with a specific error message of the contained [`esp_err_t`].
+    #[track_caller]
     pub fn panic(&self) {
         panic!("ESP-IDF ERROR: {}", self);
     }
