07ec29c50883891f301f4f456439fec8270b3d33
Ivan Markov <ivan.markov@gmail.com>
2022-08-27 05:51:05 +0000

    Fix examples

--- examples/button.rs
+++ examples/button.rs
@@ -10,9 +10,6 @@
 use std::thread;
 use std::time::Duration;
 
-use embedded_hal::digital::blocking::InputPin;
-use embedded_hal::digital::blocking::OutputPin;
-
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 
@@ -20,8 +17,8 @@ fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::new(peripherals.pins.gpio4).into_output()?;
-    let mut button = PinDriver::new(peripherals.pins.gpio9).into_input()?;
+    let mut led = PinDriver::new(peripherals.pins.gpio4)?.into_output()?;
+    let mut button = PinDriver::new(peripherals.pins.gpio9)?.into_input()?;
 
     button.set_pull_down()?;
 
