768d898177cdee463f1882c900a4e32baae0356c
Ivan Markov <ivan.markov@gmail.com>
2022-09-01 11:06:36 +0000

    Util to customize thread spawning
    Util to customize thread spawning
    
    Util to customize thread spawning
    
    Util to customize thread spawning

--- src/task.rs
+++ src/task.rs
@@ -141,3 +141,93 @@ pub unsafe fn notify(task: TaskHandle_t, notification: u32) -> bool {
 
     notified != 0
 }
+
+#[cfg(esp_idf_comp_pthread_enabled)]
+pub mod thread {
+    use esp_idf_sys::*;
+
+    use crate::cpu::Core;
+
+    #[derive(Debug)]
+    pub struct ThreadSpawnConfiguration {
+        pub name: &'static [u8],
+        pub stack_size: usize,
+        pub priority: u8,
+        pub inherit: bool,
+        pub pin_to_core: Option<Core>,
+    }
+
+    impl ThreadSpawnConfiguration {
+        pub fn get() -> Option<Self> {
+            get_conf()
+        }
+
+        pub fn set(&self) -> Result<(), EspError> {
+            set_conf(self)
+        }
+    }
+
+    impl Default for ThreadSpawnConfiguration {
+        fn default() -> Self {
+            get_default_conf()
+        }
+    }
+
+    impl From<&ThreadSpawnConfiguration> for esp_pthread_cfg_t {
+        fn from(conf: &ThreadSpawnConfiguration) -> Self {
+            Self {
+                thread_name: conf.name.as_ptr() as _,
+                stack_size: conf.stack_size as _,
+                prio: conf.priority as _,
+                inherit_cfg: conf.inherit,
+                pin_to_core: conf
+                    .pin_to_core
+                    .map(Into::into)
+                    .unwrap_or(tskNO_AFFINITY as _),
+            }
+        }
+    }
+
+    impl From<esp_pthread_cfg_t> for ThreadSpawnConfiguration {
+        fn from(conf: esp_pthread_cfg_t) -> Self {
+            Self {
+                name: unsafe {
+                    core::slice::from_raw_parts(
+                        conf.thread_name as _,
+                        strlen(conf.thread_name) as usize + 1,
+                    )
+                },
+                stack_size: conf.stack_size as _,
+                priority: conf.prio as _,
+                inherit: conf.inherit_cfg,
+                pin_to_core: if conf.pin_to_core == tskNO_AFFINITY as _ {
+                    None
+                } else {
+                    Some(conf.pin_to_core.into())
+                },
+            }
+        }
+    }
+
+    fn get_default_conf() -> ThreadSpawnConfiguration {
+        unsafe { esp_pthread_get_default_config() }.into()
+    }
+
+    fn get_conf() -> Option<ThreadSpawnConfiguration> {
+        let mut conf: esp_pthread_cfg_t = Default::default();
+
+        let res = unsafe { esp_pthread_get_cfg(&mut conf as _) };
+
+        if res == ESP_ERR_NOT_FOUND {
+            None
+        } else {
+            Some(conf.into())
+        }
+    }
+
+    fn set_conf(conf: &ThreadSpawnConfiguration) -> Result<(), EspError> {
+        esp!(unsafe { esp_pthread_set_cfg(&conf.into()) })?;
+
+        Ok(())
+    }
+}
