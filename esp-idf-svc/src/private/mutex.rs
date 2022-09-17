500aa10ba981a3003dc81059004e88320f475939
ivmarkov <ivan.markov@gmail.com>
2022-09-17 13:39:44 +0300

    just use the time_t typedef

--- esp-idf-svc/src/private/mutex.rs
+++ esp-idf-svc/src/private/mutex.rs
@@ -85,14 +85,8 @@ impl RawCondvar {
         let mut now: timeval = core::mem::zeroed();
         gettimeofday(&mut now, core::ptr::null_mut());
 
-        #[cfg(not(esp_idf_version_major = "4"))]
-        type TvSec = i64;
-
-        #[cfg(esp_idf_version_major = "4")]
-        type TvSec = i32;
-
         let abstime = timespec {
-            tv_sec: now.tv_sec + duration.as_secs() as TvSec,
+            tv_sec: now.tv_sec + duration.as_secs() as esp_idf_sys::time_t,
             tv_nsec: (now.tv_usec * 1000) + duration.subsec_nanos() as i32,
         };
 
