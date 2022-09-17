8b06fa40d17b3272bd5c5b3743c5f9214c78f942
ivmarkov <ivan.markov@gmail.com>
2022-09-17 21:05:32 +0300

    GATs stabilized

--- esp-idf-svc/src/lib.rs
+++ esp-idf-svc/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(cfg_version)]
 #![feature(const_btree_new)]
-#![feature(generic_associated_types)] // For mutex, http, http::client, http::server, ota
+#![cfg_attr(not(version("1.65")), feature(generic_associated_types))] // For mutex, http, http::client, http::server, ota
 #![cfg_attr(version("1.61"), allow(deprecated_where_clause_location))]
 #![allow(unused_imports)]
 #[cfg(any(feature = "alloc"))]
