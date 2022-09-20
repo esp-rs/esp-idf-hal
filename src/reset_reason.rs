60e11104b8ec7a12eb0b557afb8857cf4955b348
Cody P Schafer <dev@codyps.com>
2022-09-20 13:09:29 -0400

    reset_reason: add docs, use unreachable!(), use associated `pub fn get()`

--- src/reset_reason.rs
+++ src/reset_reason.rs
@@ -1,17 +1,31 @@
+//! Reset reasons
 use esp_idf_sys::*;
 
+/// Reset reasons
+#[doc(alias = "esp_reset_reason_t")]
 #[derive(Debug)]
 pub enum ResetReason {
+    /// Software restart via `esp_restart()`
     Software,
+    /// Reset by external pin
     ExternalPin,
+    /// Reset due to other watchdogs
     Watchdog,
+    /// Reset over SDIO
     Sdio,
+    /// Software reset due to exception/panic
     Panic,
+    /// Reset (software or hardware) due to interrupt watchdog
     InterruptWatchdog,
+    /// Reset due to power-on event
     PowerOn,
+    /// Reset reason can not be determined
     Unknown,
+    /// Brownout reset (software or hardware)
     Brownout,
+    /// Reset due to task watchdog
     TaskWatchdog,
+    /// Reset after exiting deep sleep mode
     DeepSleep,
 }
 
@@ -30,12 +44,20 @@ impl From<esp_reset_reason_t> for ResetReason {
             esp_reset_reason_t_ESP_RST_BROWNOUT => ResetReason::Brownout,
             esp_reset_reason_t_ESP_RST_TASK_WDT => ResetReason::TaskWatchdog,
             esp_reset_reason_t_ESP_RST_DEEPSLEEP => ResetReason::DeepSleep,
-            _ => panic!(),
+            _ => unreachable!(),
         }
     }
 }
 
-pub fn reset_reason() -> ResetReason {
-    let rr = unsafe { esp_reset_reason() };
-    rr.into()
+impl ResetReason {
+    /// Get the reason for the last reset
+    #[doc(alias = "esp_reset_reason")]
+    pub fn get() -> ResetReason {
+        // SAFETY: `esp_reset_reason()` reads from static memory that is initialized when
+        // constructors run and never modified after that point. We would only see
+        // uninitialized/partially initialized data if we run before main() (and before
+        // constructors running).
+        let rr = unsafe { esp_reset_reason() };
+        rr.into()
+    }
 }
