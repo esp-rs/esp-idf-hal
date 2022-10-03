a4df35c377e6c56831ee0b9e0ad504875595e1fa
Akos Vandra <axos88@gmail.com>
2022-10-03 07:01:30 +0200

    fix patches

--- esp-idf-sys/build/common.rs
+++ esp-idf-sys/build/common.rs
@@ -17,14 +17,14 @@ pub const V_4_3_2_PATCHES: &[&str] = &[
     "patches/missing_riscv_atomics_fix.diff",
     "patches/missing_xtensa_atomics_fix.diff",
     "patches/pthread_destructor_fix.diff",
-    "patches/esp_app_format_weak.diff",
+    "patches/esp_app_format_weak_v4.3.diff",
 ];
 
 #[allow(dead_code)]
-pub const MASTER_PATCHES: &[&str] = &["patches/esp_app_format_weak.diff"];
+pub const MASTER_PATCHES: &[&str] = &["patches/esp_app_format_weak_v5.diff"];
 
 #[allow(dead_code)]
-pub const V_4_4_2_PATCHES: &[&str] = &["patches/esp_app_format_weak.diff"];
+pub const V_4_4_2_PATCHES: &[&str] = &["patches/esp_app_format_weak_v4.4.diff"];
 
 const TOOLS_WORKSPACE_INSTALL_DIR: &str = ".embuild";
 
