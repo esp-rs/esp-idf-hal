b6edbf010ea2ca1471c932230ea73729d945ffbc
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2022-11-05 12:41:15 +0100

    docs: 🐛 Fix docstrings (#150)

--- esp-idf-sys/build/native/cargo_driver/chip.rs
+++ esp-idf-sys/build/native/cargo_driver/chip.rs
@@ -8,13 +8,13 @@ use embuild::espidf::EspIdfVersion;
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Display, EnumString)]
 #[repr(u32)]
 pub enum Chip {
-    /// Xtensa LX7 based dual core
+    /// Xtensa LX6 based dual core
     #[strum(serialize = "esp32")]
     ESP32 = 0,
     /// Xtensa LX7 based single core
     #[strum(serialize = "esp32s2")]
     ESP32S2,
-    /// Xtensa LX7 based single core
+    /// Xtensa LX7 based dual core
     #[strum(serialize = "esp32s3")]
     ESP32S3,
     /// RISC-V based single core
