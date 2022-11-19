7a4d027affdf19967dd2b1840c8cdbcbe717c5dc
Ronen Ulanovsky <ronen.ulanovsky@nanit.com>
2022-11-19 22:47:03 +0200

    Update default ESP-IDF version in native builds to `v4.4.3`

--- esp-idf-sys/build/native/cargo_driver/config.rs
+++ esp-idf-sys/build/native/cargo_driver/config.rs
@@ -15,7 +15,7 @@ use crate::config::EspIdfSys;
 pub const ESP_IDF_VERSION_VAR: &str = "ESP_IDF_VERSION";
 pub const ESP_IDF_REPOSITORY_VAR: &str = "ESP_IDF_REPOSITORY";
 
-pub const DEFAULT_ESP_IDF_VERSION: &str = "v4.4.1";
+pub const DEFAULT_ESP_IDF_VERSION: &str = "v4.4.3";
 pub const DEFAULT_CMAKE_GENERATOR: cmake::Generator = {
     // No Ninja builds for linux=aarch64 from Espressif yet
     #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
