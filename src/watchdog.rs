9f0bb8af3f8084873f1a0f0456f19d3de98ed4c1
Alexander Hübener <alex.teamplayer@gmail.com>
2022-12-04 22:43:16 +0100

    updated api

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -4,26 +4,31 @@ pub mod task {
     //!
     //! ```rust, ignore
     //! # fn main() -> Result<()> {
-    //! let mut watchdog = TaskWatchdog::take()?.unwrap();
+    //! let peripherals = Peripherals::take().unwrap();
     //!
-    //! watchdog.reconfigure(WatchdogConfig {
-    //!     duration: Duration::from_secs(2),
-    //!     panic_on_trigger: true,
-    //!     ..Default::default()
-    //! })?;
+    //! let mut driver = esp_idf_hal::watchdog::task::TWDTDriver::new(
+    //!     peripherals.twdt,
+    //!     WatchdogConfig {
+    //!         duration: Duration::from_secs(2),
+    //!         panic_on_trigger: true,
+    //!         ..Default::default()
+    //!     },
+    //! )?;
     //!
-    //! let task = Task::current();
-    //!
-    //! task.set_watchdog(&mut watchdog)?;
+    //! let mut watchdog = driver.watch_current_task()?;
     //!
     //! loop {
-    //!     task.reset_watchdog()?;
+    //!     watchdog.feed();
     //!     unsafe { vTaskDelay(1) };
     //! }
     //! # }
     //! ```
 
+    use core::{marker::PhantomData, sync::atomic::AtomicUsize};
+
     use esp_idf_sys::*;
+
+    use crate::peripheral::Peripheral;
 
     pub struct WatchdogConfig {
         pub duration: core::time::Duration,
@@ -36,7 +41,7 @@ pub mod task {
         fn default() -> Self {
             Self {
                 duration: core::time::Duration::from_secs(CONFIG_ESP_TASK_WDT_TIMEOUT_S as u64),
-                panic_on_trigger: false, // CONFIG_ESP_TASK_WDT_PANIC
+                panic_on_trigger: cfg!(esp_idf_esp_task_wdt_panic), // CONFIG_ESP_TASK_WDT_PANIC
                 #[cfg(not(esp_idf_version_major = "4"))]
                 subscribed_idle_tasks: {
                     let subscribed_idle_tasks = heapless::Vec::new();
@@ -71,60 +76,75 @@ pub mod task {
         }
     }
 
-    static TAKEN: core::sync::atomic::AtomicBool = core::sync::atomic::AtomicBool::new(false);
+    pub trait Twdt {}
 
-    static TAKEN_CS: crate::task::CriticalSection = crate::task::CriticalSection::new();
+    pub struct TWDTDriver<'d>(PhantomData<&'d mut ()>);
 
-    #[derive(Debug, Clone)]
-    pub struct TaskWatchdog;
+    unsafe impl Send for TWDTDriver<'_> {}
 
-    impl TaskWatchdog {
-        pub fn take() -> Result<Option<Self>, EspError> {
-            if TAKEN.load(core::sync::atomic::Ordering::SeqCst) {
-                Ok(None)
-            } else {
-                let _ = TAKEN_CS.enter();
+    static TWDT_DRIVER_REF_COUNT: AtomicUsize = AtomicUsize::new(0);
 
-                if !TAKEN.load(core::sync::atomic::Ordering::SeqCst) {
-                    TAKEN.store(true, core::sync::atomic::Ordering::SeqCst);
-
-                    Ok(Some(Self::init()?))
-                } else {
-                    Ok(None)
-                }
-            }
-        }
-
-        fn init() -> Result<Self, EspError> {
-            if !is_initialized() {
-                init_or_reconfigure(WatchdogConfig::default())?;
-            }
-
-            Ok(TaskWatchdog)
-        }
-
-        pub fn reconfigure(&mut self, config: WatchdogConfig) -> Result<(), EspError> {
-            init_or_reconfigure(config)
-        }
-
-        pub(crate) fn add_task(&mut self, task: TaskHandle_t) -> Result<(), EspError> {
-            add_task(task)
-        }
-
-        pub(crate) fn remove_task(&mut self, task: TaskHandle_t) {
-            // can be unwrapped, error occurs only if watchdog not initialized yet and this
-            // shouldn't be the case at all
-            remove_task(task).unwrap()
-        }
-
-        pub(crate) fn is_enabled_for_task(&self, task: TaskHandle_t) -> bool {
-            is_enabled_for_task(task).unwrap()
-        }
-
-        pub(crate) fn reset_current() -> Result<(), EspError> {
-            reset_timer()
+    impl Clone for TWDTDriver<'_> {
+        fn clone(&self) -> Self {
+            TWDT_DRIVER_REF_COUNT.fetch_add(1, core::sync::atomic::Ordering::Relaxed);
+            Self(Default::default())
         }
     }
+
+    impl<'d> TWDTDriver<'d> {
+        pub fn new(
+            _twdt: impl Peripheral<P = TWDT> + 'd,
+            config: WatchdogConfig,
+        ) -> Result<Self, EspError> {
+            init_or_reconfigure(config)?;
+            Ok(Self(Default::default()))
+        }
+
+        // will fail in isr
+        pub fn watch_current_task<'a>(&'a mut self) -> Result<WatchdogSubscription<'a>, EspError> {
+            add_task(core::ptr::null_mut())?;
+            Ok(WatchdogSubscription::new())
+        }
+    }
+
+    impl Drop for TWDTDriver<'_> {
+        fn drop(&mut self) {
+            let drop = TWDT_DRIVER_REF_COUNT
+                .fetch_update(
+                    core::sync::atomic::Ordering::Relaxed,
+                    core::sync::atomic::Ordering::Relaxed,
+                    |count| if count == 0 { None } else { Some(count - 1) },
+                )
+                .is_err();
+            if drop {
+                // TODO: maybe deinit twdt from idf (only if not initialized by idf)
+                println!("TWDTDriver dropped")
+            }
+        }
+    }
+
+    pub struct WatchdogSubscription<'s>(PhantomData<&'s mut ()>);
+
+    impl WatchdogSubscription<'_> {
+        fn new() -> Self {
+            Self(Default::default())
+        }
+    }
+
+    impl embedded_hal_0_2::watchdog::Watchdog for WatchdogSubscription<'_> {
+        fn feed(&mut self) {
+            feed_from_curr_task().unwrap()
+        }
+    }
+
+    impl Drop for WatchdogSubscription<'_> {
+        fn drop(&mut self) {
+            let _ = remove_task(core::ptr::null_mut());
+        }
+    }
+
+    crate::impl_peripheral!(TWDT);
+    impl Twdt for TWDT {}
 
     fn init_or_reconfigure(config: WatchdogConfig) -> Result<(), EspError> {
         unsafe {
@@ -135,15 +155,12 @@ pub mod task {
         }
     }
 
-    fn reset_timer() -> Result<(), EspError> {
+    fn feed_from_curr_task() -> Result<(), EspError> {
         unsafe { esp!(esp_task_wdt_reset()) }
     }
 
     fn add_task(task: TaskHandle_t) -> Result<(), EspError> {
-        match unsafe { esp_task_wdt_add(task) } {
-            ESP_ERR_INVALID_ARG | ESP_OK => Ok(()),
-            e => EspError::convert(e),
-        }
+        unsafe { esp!(esp_task_wdt_add(task)) }
     }
 
     fn remove_task(task: TaskHandle_t) -> Result<(), EspError> {
@@ -153,20 +170,20 @@ pub mod task {
         }
     }
 
-    fn is_enabled_for_task(task: TaskHandle_t) -> Result<bool, EspError> {
-        let is_enabled = match unsafe { esp_task_wdt_status(task) } {
-            ESP_ERR_NOT_FOUND => false,
-            ESP_OK => true,
-            e => return Err(EspError::from(e).unwrap()),
-        };
+    // fn is_enabled_for_task(task: TaskHandle_t) -> Result<bool, EspError> {
+    //     let is_enabled = match unsafe { esp_task_wdt_status(task) } {
+    //         ESP_ERR_NOT_FOUND => false,
+    //         ESP_OK => true,
+    //         e => return Err(EspError::from(e).unwrap()),
+    //     };
 
-        Ok(is_enabled)
-    }
+    //     Ok(is_enabled)
+    // }
 
-    pub fn is_initialized() -> bool {
-        !matches!(
-            unsafe { esp_task_wdt_status(core::ptr::null_mut()) },
-            ESP_ERR_INVALID_STATE
-        )
-    }
+    // fn is_initialized() -> bool {
+    //     !matches!(
+    //         unsafe { esp_task_wdt_status(core::ptr::null_mut()) },
+    //         ESP_ERR_INVALID_STATE
+    //     )
+    // }
 }
