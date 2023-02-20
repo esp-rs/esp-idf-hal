d477d54e2b3e7e44c9d9420b17ddec3b0a447bf3
Danilo Bargen <mail@dbrgn.ch>
2023-02-20 14:45:41 +0100

    General purpose delay provider (#210)
    * Move delay implementations to dedicated subdirectory
    
    * Add general purpose delay provider

---
+++ src/delay/general_purpose.rs
@@ -0,0 +1,48 @@
+use crate::delay::{Ets, FreeRtos};
+
+/// A delay provider that uses [`Ets`] for delays <10ms, and [`FreeRtos`] for
+/// delays >=10 ms
+#[derive(Copy, Clone)]
+pub struct Delay;
+
+impl Delay {
+    pub fn delay_us(us: u32) {
+        if us < 10_000 {
+            Ets::delay_us(us);
+        } else {
+            FreeRtos::delay_us(us);
+        }
+    }
+
+    pub fn delay_ms(ms: u32) {
+        if ms < 10 {
+            Ets::delay_ms(ms);
+        } else {
+            FreeRtos::delay_ms(ms);
+        }
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayUs<u16> for Delay {
+    fn delay_us(&mut self, us: u16) {
+        Delay::delay_us(us as _);
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        Delay::delay_us(us);
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayMs<u16> for Delay {
+    fn delay_ms(&mut self, ms: u16) {
+        Delay::delay_ms(ms as _)
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        Delay::delay_ms(ms)
+    }
+}
