0f2142fdd7cf89164bed19a3d42eac5cdcb88e1e
ivmarkov <ivan.markov@gmail.com>
2023-05-12 23:31:03 +0000

    Enable ws client bindings only if WS transport is configured

--- esp-idf-svc/src/ws.rs
+++ esp-idf-svc/src/ws.rs
@@ -1,4 +1,4 @@
 //! WebSocket protocol
 
-#[cfg(all(feature = "alloc", esp_idf_version_major = "4"))]
+#[cfg(all(feature = "alloc", esp_idf_version_major = "4", esp_idf_ws_transport))]
 pub mod client;
