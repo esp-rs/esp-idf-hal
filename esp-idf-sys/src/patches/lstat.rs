164cbdbc1e7637ea01e54036860ebb74096cae35
ivmarkov <ivan.markov@gmail.com>
2023-05-12 19:09:35 +0000

    esp-idf-template#118

--- esp-idf-sys/src/patches/lstat.rs
+++ esp-idf-sys/src/patches/lstat.rs
@@ -1,4 +1,4 @@
-use crate::*;
+use crate::stat;
 use core::ffi;
 
 static mut __LSTAT_INTERNAL_REFERENCE: *mut ffi::c_void = lstat as *mut _;
@@ -10,5 +10,13 @@ pub fn link_patches() -> *mut ffi::c_void {
 #[no_mangle]
 #[inline(never)]
 pub unsafe extern "C" fn lstat(path: *const ffi::c_char, buf: *mut stat) -> ffi::c_int {
-    stat(path, buf)
+    extern "C" {
+        // Declare it manually, because if certain ESP IDF components are not included (VFS?)
+        // as is the case for CMake based builds, the signature of the `stat` fn is actually
+        // not defined in the `esp-idf-sys` bindings
+        #[link_name = "stat"]
+        fn esp_idf_sys_stat(path: *const ffi::c_char, buf: *mut stat) -> ffi::c_int;
+    }
+
+    esp_idf_sys_stat(path, buf)
 }
