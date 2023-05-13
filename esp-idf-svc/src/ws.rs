f0023709ba2ba4d99645f4fe9c9035ae978bb034
ivmarkov <ivan.markov@gmail.com>
2023-05-13 11:04:10 +0000

    Try to fix the CMake ESP IDF 4.4 build

--- esp-idf-svc/src/ws.rs
+++ esp-idf-svc/src/ws.rs
@@ -1,4 +1,10 @@
 //! WebSocket protocol
 
-#[cfg(all(feature = "alloc", esp_idf_version_major = "4", esp_idf_ws_transport))]
+#[cfg(all(
+    feature = "alloc",
+    esp_idf_version_major = "4",
+    esp_idf_ws_transport,
+    esp_idf_comp_tcp_transport_enabled,
+    esp_idf_comp_esp_tls_enabled
+))]
 pub mod client;
