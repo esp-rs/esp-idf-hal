d55afcabea63a88766c15911cc1a39e6b07ffe0c
Frederick <frederick@vollbrecht.cc>
2023-07-05 17:33:28 +0200

    remove esp-idf-svc dep on logging

--- examples/timer_notify.rs
+++ examples/timer_notify.rs
@@ -4,8 +4,6 @@ fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
 
     let per = esp_idf_hal::peripherals::Peripherals::take().unwrap();
 
