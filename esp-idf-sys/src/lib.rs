a3b08d72ccf60ae5d1dcaf4f48af427fd55371ef
David Cuthbert <dacut@kanga.org>
2023-08-23 09:14:59 -0700

    PCNT v5 bindings take 2 (#230)
    * Provide PCNT v5 bindings.
    
    This provides bindings for the PCNT peripheral using the ESP-IDF v5+
    definitions in conjunction with the ESP-IDF v4 definitions. It removes
    a case where bindgen can't cope with a typename that exists in two
    namespaces (enum pcnt_unit_t in v4, struct pcnt_unit_t in v5) by
    manually handling this special case.
    
    * Fix PCNT bindings.
    
    This fixes the PCNT bindings so that they work with esp-idf-hal,
    and actually tested against chips that *have* PCNT peripherals this
    time.

--- esp-idf-sys/src/lib.rs
+++ esp-idf-sys/src/lib.rs
@@ -14,6 +14,10 @@
 pub use bindings::*;
 pub use error::*;
 
+// Don't use esp_idf_soc_pcnt_supported; that's only on ESP-IDF v5.x+.
+#[cfg(any(esp32, esp32s2, esp32s3, esp32c6, esp32h2))]
+pub use pcnt::*;
+
 #[doc(hidden)]
 pub use build_time;
 #[doc(hidden)]
@@ -26,6 +30,9 @@ mod app_desc;
 mod error;
 mod panic;
 mod patches;
+#[cfg(any(esp32, esp32s2, esp32s3, esp32c6, esp32h2))]
+mod pcnt;
+
 mod start;
 
 /// If any of the two constants below do not compile, you have not properly setup the rustc cfg flag `espidf_time64`:
@@ -59,5 +66,8 @@ pub fn link_patches() -> PatchesRef {
 #[allow(rustdoc::all)]
 #[allow(improper_ctypes)] // TODO: For now, as 5.0 spits out tons of these
 mod bindings {
+    #[cfg(any(esp32, esp32s2, esp32s3, esp32c6, esp32h2))]
+    use crate::pcnt::*;
+
     include!(env!("EMBUILD_GENERATED_BINDINGS_FILE"));
 }
