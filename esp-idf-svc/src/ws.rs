f6b237b5f6be89353cbe10984debe7d8f298b361
ivmarkov <ivan.markov@gmail.com>
2023-08-31 06:56:54 +0000

    Prep websocket client for the upcoming ESP IDF 5 support

--- esp-idf-svc/src/ws.rs
+++ esp-idf-svc/src/ws.rs
@@ -2,9 +2,11 @@
 
 #[cfg(all(
     feature = "alloc",
-    esp_idf_version_major = "4",
-    esp_idf_ws_transport,
     esp_idf_comp_tcp_transport_enabled,
-    esp_idf_comp_esp_tls_enabled
+    esp_idf_comp_esp_tls_enabled,
+    any(
+        all(esp_idf_version_major = "4", esp_idf_ws_transport),
+        esp_idf_comp_espressif__esp_websocket_client_enabled
+    )
 ))]
 pub mod client;
