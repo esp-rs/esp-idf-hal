8ded86eb20d5e58f038f67bf29d1f0a5f515deec
ivmarkov <ivan.markov@gmail.com>
2023-09-02 18:13:34 +0000

    Unite all delay impls in a single module

--- src/delay.rs
+++ src/delay.rs
@@ -8,10 +8,6 @@ use core::{cmp::min, time::Duration};
 
 use esp_idf_sys::*;
 
-mod general_purpose;
-
-pub use general_purpose::Delay;
-
 #[allow(non_upper_case_globals)]
 pub const BLOCK: TickType_t = TickType_t::MAX;
 
@@ -229,5 +225,62 @@ impl embedded_hal::delay::DelayUs for FreeRtos {
 
     fn delay_ms(&mut self, ms: u32) {
         FreeRtos::delay_ms(ms)
+    }
+}
+
+/// A delay provider that uses [`Ets`] for delays <10ms, and [`FreeRtos`] for
+/// delays >=10 ms
+#[derive(Copy, Clone)]
+pub struct Delay;
+
+impl Delay {
+    pub fn delay_us(us: u32) {
+        if us < 10_000 {
+            Ets::delay_us(us);
+        } else {
+            FreeRtos::delay_us(us);
+        }
+    }
+
+    pub fn delay_ms(ms: u32) {
+        if ms < 10 {
+            Ets::delay_ms(ms);
+        } else {
+            FreeRtos::delay_ms(ms);
+        }
+    }
+}
+
+impl embedded_hal::delay::DelayUs for Delay {
+    fn delay_us(&mut self, us: u32) {
+        Delay::delay_us(us)
+    }
+
+    fn delay_ms(&mut self, ms: u32) {
+        Delay::delay_ms(ms)
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayUs<u16> for Delay {
+    fn delay_us(&mut self, us: u16) {
+        Delay::delay_us(us as _);
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        Delay::delay_us(us);
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayMs<u16> for Delay {
+    fn delay_ms(&mut self, ms: u16) {
+        Delay::delay_ms(ms as _)
+    }
+}
+
+impl embedded_hal_0_2::blocking::delay::DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        Delay::delay_ms(ms)
     }
 }
