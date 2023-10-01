751b7b1c3af96b97cdf086aaa5d5cef6826b6244
ok32 <artuh.gubanova@gmail.com>
2023-10-01 17:59:23 +0400

    Add BLE mesh bindings (#244)
    * Add BLE mesh bindings
    
    * Move the BLE mesh includes inside CONFIG_BT_ENABLED
    
    ---------
    
    Co-authored-by: ok32 <e.starikoff@gmail.com>

--- esp-idf-sys/src/include/esp-idf/bindings.h
+++ esp-idf-sys/src/include/esp-idf/bindings.h
@@ -393,6 +393,18 @@
 #include "services/gatt/ble_svc_gatt.h"
 #endif // CONFIG_BT_NIMBLE_ENABLED
 
+// BLE Mesh
+#ifdef CONFIG_BLE_MESH
+#include "esp_ble_mesh_defs.h"
+#include "esp_ble_mesh_ble_api.h"
+#include "esp_ble_mesh_common_api.h"
+#include "esp_ble_mesh_local_data_operation_api.h"
+#include "esp_ble_mesh_low_power_api.h"
+#include "esp_ble_mesh_networking_api.h"
+#include "esp_ble_mesh_provisioning_api.h"
+#include "esp_ble_mesh_proxy_api.h"
+#endif // CONFIG_BLE_MESH
+
 #endif // CONFIG_BT_ENABLED
 
 #endif // CONFIG_IDF_TARGET_ESP32S2
