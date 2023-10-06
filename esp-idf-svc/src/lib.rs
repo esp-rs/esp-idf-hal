f6cd82e89a9ab776c8080cfec3a297182c1bfc0e
ivmarkov <ivan.markov@gmail.com>
2023-10-06 19:31:08 +0000

    fix nightly CI

--- esp-idf-svc/src/lib.rs
+++ esp-idf-svc/src/lib.rs
@@ -11,13 +11,12 @@
 //! - `embassy-time-driver`
 //! - `embassy-time-isr-queue`
 #![cfg_attr(not(feature = "std"), no_std)]
-#![allow(stable_features)]
 #![feature(cfg_version)]
-#![cfg_attr(
-    feature = "nightly",
-    feature(async_fn_in_trait, impl_trait_projections)
-)]
-
+#![allow(stable_features)]
+#![allow(unknown_lints)]
+#![cfg_attr(feature = "nightly", feature(async_fn_in_trait))]
+#![cfg_attr(feature = "nightly", allow(async_fn_in_trait))]
+#![cfg_attr(feature = "nightly", feature(impl_trait_projections))]
 #[cfg(feature = "alloc")]
 #[allow(unused_imports)]
 #[macro_use]
