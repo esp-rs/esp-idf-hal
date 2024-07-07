d1ace4b79aeadde48aafc7ca76610260fced6572
ivmarkov <ivan.markov@gmail.com>
2024-07-07 18:33:28 +0300

    Add imac targets to CI + fix ESP IDF 4.4.x builds for imac target (#450)

--- src/gpio.rs
+++ src/gpio.rs
@@ -1448,7 +1448,7 @@ fn gpio_reset_without_pull(pin: gpio_num_t) -> Result<(), EspError> {
         pull_up_en: esp_idf_sys::gpio_pullup_t_GPIO_PULLUP_DISABLE,
         pull_down_en: esp_idf_sys::gpio_pulldown_t_GPIO_PULLDOWN_DISABLE,
         intr_type: esp_idf_sys::gpio_int_type_t_GPIO_INTR_DISABLE,
-        #[cfg(esp32h2)]
+        #[cfg(all(esp32h2, not(esp_idf_version_major = "4")))]
         hys_ctrl_mode: esp_idf_sys::gpio_hys_ctrl_mode_t_GPIO_HYS_SOFT_DISABLE,
     };
 
