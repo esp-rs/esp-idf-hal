426a058bc7ca7284f82c84ae5a9423677babe041
ivmarkov <ivan.markov@gmail.com>
2024-10-04 07:05:24 +0000

    Clippy

--- esp-idf-sys/build/config.rs
+++ esp-idf-sys/build/config.rs
@@ -229,7 +229,7 @@ pub mod utils {
             fields: &'a mut Option<&'static [&'static str]>,
         }
 
-        impl<'de, 'a> Deserializer<'de> for StructFieldsDeserializer<'a> {
+        impl<'de> Deserializer<'de> for StructFieldsDeserializer<'_> {
             type Error = serde::de::value::Error;
 
             fn deserialize_any<V>(self, _visitor: V) -> Result<V::Value, Self::Error>
