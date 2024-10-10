4d59fbe758867b5947cdc3e616a4571c27e60e3a
Small Brain <smallbraindev@gmail.com>
2024-10-10 13:11:44 -0700

    Typo fix in task docs (#489)
    - small typo fix "fo" -> "for" in task docs

--- src/task.rs
+++ src/task.rs
@@ -38,7 +38,7 @@ const NO_AFFINITY: core::ffi::c_uint = tskNO_AFFINITY;
 ///
 /// # Safety
 ///
-/// Only marked as unsafe fo symmetry with `destroy` and to discourage users from leaning on it in favor of `std::thread`.
+/// Only marked as unsafe for symmetry with `destroy` and to discourage users from leaning on it in favor of `std::thread`.
 /// Otherwise, this function is actually safe.
 pub unsafe fn create(
     task_handler: extern "C" fn(*mut core::ffi::c_void),
