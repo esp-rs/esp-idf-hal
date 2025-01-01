d6f11649aedf9b75ae3dce23d4473bd8c72dff82
ivmarkov <ivan.markov@gmail.com>
2025-01-01 19:01:57 +0000

    Fix CI; remove warnings from the example

--- examples/temperature_sensor.rs
+++ examples/temperature_sensor.rs
@@ -1,9 +1,9 @@
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::peripherals::Peripherals;
-
 #[cfg(all(esp_idf_soc_temp_sensor_supported, esp_idf_version_major = "5"))]
 fn main() -> anyhow::Result<()> {
+    use esp_idf_hal::delay::FreeRtos;
+    use esp_idf_hal::peripherals::Peripherals;
     use esp_idf_hal::temp_sensor::*;
+
     esp_idf_hal::sys::link_patches();
 
     let peripherals = Peripherals::take()?;
