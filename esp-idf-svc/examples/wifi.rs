7d2cb7b1e82477e54e1665be2b8ab38eaec4e270
ivmarkov <ivan.markov@gmail.com>
2025-04-28 06:21:40 +0000

    Clippy

--- esp-idf-svc/examples/wifi.rs
+++ esp-idf-svc/examples/wifi.rs
@@ -33,7 +33,7 @@ fn main() -> anyhow::Result<()> {
 
     let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
 
-    info!("Wifi DHCP info: {:?}", ip_info);
+    info!("Wifi DHCP info: {ip_info:?}");
 
     info!("Shutting down in 5s...");
 
