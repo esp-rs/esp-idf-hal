0ace025e0e90d4b2663b86dbdf1d50cec1917fc4
ivmarkov <ivan.markov@gmail.com>
2025-06-03 12:26:45 +0000

    Remove Peripheral/PeripheralRef

--- src/sd/mmc.rs
+++ src/sd/mmc.rs
@@ -1,9 +1,7 @@
 use core::marker::PhantomData;
-use core::ops::Deref;
 use core::sync::atomic::{AtomicU8, Ordering};
 
 use crate::gpio::{self, InputPin, OutputPin};
-use crate::peripheral::Peripheral;
 use crate::sys::*;
 
 /// Indicates that card detect line is not used
@@ -69,13 +67,13 @@ impl<'d> SdMmcHostDriver<'d> {
     /// Create a new driver for the provided slot peripheral with data line width 1.
     #[cfg(esp_idf_soc_sdmmc_use_gpio_matrix)]
     #[allow(clippy::too_many_arguments)]
-    pub fn new_1bit<S: SdMmc>(
-        slot: impl Peripheral<P = S> + 'd,
-        cmd: impl Peripheral<P = impl OutputPin> + 'd,
-        clk: impl Peripheral<P = impl OutputPin> + 'd,
-        d0: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+    pub fn new_1bit(
+        slot: impl SdMmc + 'd,
+        cmd: impl OutputPin + 'd,
+        clk: impl OutputPin + 'd,
+        d0: impl InputPin + OutputPin + 'd,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -100,16 +98,16 @@ impl<'d> SdMmcHostDriver<'d> {
     /// Create a new driver for the provided slot peripheral with data line width 4.
     #[cfg(esp_idf_soc_sdmmc_use_gpio_matrix)]
     #[allow(clippy::too_many_arguments)]
-    pub fn new_4bits<S: SdMmc>(
-        slot: impl Peripheral<P = S> + 'd,
-        cmd: impl Peripheral<P = impl OutputPin> + 'd,
-        clk: impl Peripheral<P = impl OutputPin> + 'd,
-        d0: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d1: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d2: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d3: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+    pub fn new_4bits<S: SdMmc + 'd>(
+        slot: S,
+        cmd: impl OutputPin + 'd,
+        clk: impl OutputPin + 'd,
+        d0: impl InputPin + OutputPin + 'd,
+        d1: impl InputPin + OutputPin + 'd,
+        d2: impl InputPin + OutputPin + 'd,
+        d3: impl InputPin + OutputPin + 'd,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -134,20 +132,20 @@ impl<'d> SdMmcHostDriver<'d> {
     /// Create a new driver for the provided slot peripheral with data line width 8.
     #[cfg(esp_idf_soc_sdmmc_use_gpio_matrix)]
     #[allow(clippy::too_many_arguments)]
-    pub fn new_8bits<S: SdMmc>(
-        slot: impl Peripheral<P = S> + 'd,
-        cmd: impl Peripheral<P = impl OutputPin> + 'd,
-        clk: impl Peripheral<P = impl OutputPin> + 'd,
-        d0: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d1: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d2: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d3: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d4: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d5: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d6: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        d7: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+    pub fn new_8bits<S: SdMmc + 'd>(
+        slot: S,
+        cmd: impl OutputPin + 'd,
+        clk: impl OutputPin + 'd,
+        d0: impl InputPin + OutputPin + 'd,
+        d1: impl InputPin + OutputPin + 'd,
+        d2: impl InputPin + OutputPin + 'd,
+        d3: impl InputPin + OutputPin + 'd,
+        d4: impl InputPin + OutputPin + 'd,
+        d5: impl InputPin + OutputPin + 'd,
+        d6: impl InputPin + OutputPin + 'd,
+        d7: impl InputPin + OutputPin + 'd,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -173,12 +171,12 @@ impl<'d> SdMmcHostDriver<'d> {
     #[cfg(not(esp_idf_soc_sdmmc_use_gpio_matrix))]
     #[allow(clippy::too_many_arguments)]
     pub fn new_slot0_1bit(
-        slot0: impl Peripheral<P = SDMMC0> + 'd,
-        cmd: impl Peripheral<P = gpio::Gpio11> + 'd,
-        clk: impl Peripheral<P = gpio::Gpio6> + 'd,
-        d0: impl Peripheral<P = gpio::Gpio7> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        slot0: SDMMC0<'d>,
+        cmd: gpio::Gpio11<'d>,
+        clk: gpio::Gpio6<'d>,
+        d0: gpio::Gpio7<'d>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -204,15 +202,15 @@ impl<'d> SdMmcHostDriver<'d> {
     #[cfg(not(esp_idf_soc_sdmmc_use_gpio_matrix))]
     #[allow(clippy::too_many_arguments)]
     pub fn new_slot0_4bits(
-        slot0: impl Peripheral<P = SDMMC0> + 'd,
-        cmd: impl Peripheral<P = gpio::Gpio11> + 'd,
-        clk: impl Peripheral<P = gpio::Gpio6> + 'd,
-        d0: impl Peripheral<P = gpio::Gpio7> + 'd,
-        d1: impl Peripheral<P = gpio::Gpio8> + 'd,
-        d2: impl Peripheral<P = gpio::Gpio9> + 'd,
-        d3: impl Peripheral<P = gpio::Gpio10> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        slot0: SDMMC0<'d>,
+        cmd: gpio::Gpio11<'d>,
+        clk: gpio::Gpio6<'d>,
+        d0: gpio::Gpio7<'d>,
+        d1: gpio::Gpio8<'d>,
+        d2: gpio::Gpio9<'d>,
+        d3: gpio::Gpio10<'d>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -238,19 +236,19 @@ impl<'d> SdMmcHostDriver<'d> {
     #[cfg(not(esp_idf_soc_sdmmc_use_gpio_matrix))]
     #[allow(clippy::too_many_arguments)]
     pub fn new_slot0_8bits(
-        slot0: impl Peripheral<P = SDMMC0> + 'd,
-        cmd: impl Peripheral<P = gpio::Gpio11> + 'd,
-        clk: impl Peripheral<P = gpio::Gpio6> + 'd,
-        d0: impl Peripheral<P = gpio::Gpio7> + 'd,
-        d1: impl Peripheral<P = gpio::Gpio8> + 'd,
-        d2: impl Peripheral<P = gpio::Gpio9> + 'd,
-        d3: impl Peripheral<P = gpio::Gpio10> + 'd,
-        d4: impl Peripheral<P = gpio::Gpio16> + 'd,
-        d5: impl Peripheral<P = gpio::Gpio17> + 'd,
-        d6: impl Peripheral<P = gpio::Gpio5> + 'd,
-        d7: impl Peripheral<P = gpio::Gpio18> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        slot0: SDMMC0<'d>,
+        cmd: gpio::Gpio11<'d>,
+        clk: gpio::Gpio6<'d>,
+        d0: gpio::Gpio7<'d>,
+        d1: gpio::Gpio8<'d>,
+        d2: gpio::Gpio9<'d>,
+        d3: gpio::Gpio10<'d>,
+        d4: gpio::Gpio16<'d>,
+        d5: gpio::Gpio17<'d>,
+        d6: gpio::Gpio5<'d>,
+        d7: gpio::Gpio18<'d>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -276,12 +274,12 @@ impl<'d> SdMmcHostDriver<'d> {
     #[cfg(not(esp_idf_soc_sdmmc_use_gpio_matrix))]
     #[allow(clippy::too_many_arguments)]
     pub fn new_slot1_1bit(
-        slot1: impl Peripheral<P = SDMMC1> + 'd,
-        cmd: impl Peripheral<P = gpio::Gpio15> + 'd,
-        clk: impl Peripheral<P = gpio::Gpio14> + 'd,
-        d0: impl Peripheral<P = gpio::Gpio2> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        slot1: SDMMC1<'d>,
+        cmd: gpio::Gpio15<'d>,
+        clk: gpio::Gpio14<'d>,
+        d0: gpio::Gpio2<'d>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -307,15 +305,15 @@ impl<'d> SdMmcHostDriver<'d> {
     #[cfg(not(esp_idf_soc_sdmmc_use_gpio_matrix))]
     #[allow(clippy::too_many_arguments)]
     pub fn new_slot1_4bits(
-        slot1: impl Peripheral<P = SDMMC1> + 'd,
-        cmd: impl Peripheral<P = gpio::Gpio15> + 'd,
-        clk: impl Peripheral<P = gpio::Gpio14> + 'd,
-        d0: impl Peripheral<P = gpio::Gpio2> + 'd,
-        d1: impl Peripheral<P = gpio::Gpio4> + 'd,
-        d2: impl Peripheral<P = gpio::Gpio12> + 'd,
-        d3: impl Peripheral<P = gpio::Gpio13> + 'd,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        slot1: SDMMC1<'d>,
+        cmd: gpio::Gpio15<'d>,
+        clk: gpio::Gpio14<'d>,
+        d0: gpio::Gpio2<'d>,
+        d1: gpio::Gpio4<'d>,
+        d2: gpio::Gpio12<'d>,
+        d3: gpio::Gpio13<'d>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
         config: &config::Configuration,
     ) -> Result<Self, EspError> {
         Self::new_internal(
@@ -338,22 +336,22 @@ impl<'d> SdMmcHostDriver<'d> {
     }
 
     #[allow(clippy::too_many_arguments)]
-    fn new_internal<S: SdMmc>(
+    fn new_internal<S: SdMmc + 'd>(
         width: u8,
         internal_pullups: bool,
-        _slot: impl Peripheral<P = S> + 'd,
-        _cmd: impl Peripheral<P = impl OutputPin> + 'd,
-        _clk: impl Peripheral<P = impl OutputPin> + 'd,
-        _d0: impl Peripheral<P = impl InputPin + OutputPin> + 'd,
-        _d1: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d2: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d3: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d4: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d5: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d6: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        _d7: Option<impl Peripheral<P = impl InputPin + OutputPin> + 'd>,
-        cd: Option<impl Peripheral<P = impl InputPin> + 'd>,
-        wp: Option<impl Peripheral<P = impl InputPin> + 'd>,
+        _slot: S,
+        _cmd: impl OutputPin + 'd,
+        _clk: impl OutputPin + 'd,
+        _d0: impl InputPin + OutputPin + 'd,
+        _d1: Option<impl InputPin + OutputPin + 'd>,
+        _d2: Option<impl InputPin + OutputPin + 'd>,
+        _d3: Option<impl InputPin + OutputPin + 'd>,
+        _d4: Option<impl InputPin + OutputPin + 'd>,
+        _d5: Option<impl InputPin + OutputPin + 'd>,
+        _d6: Option<impl InputPin + OutputPin + 'd>,
+        _d7: Option<impl InputPin + OutputPin + 'd>,
+        cd: Option<impl InputPin + 'd>,
+        wp: Option<impl InputPin + 'd>,
     ) -> Result<Self, EspError> {
         let slot_config = sdmmc_slot_config_t {
             width: width as _,
@@ -363,14 +361,10 @@ impl<'d> SdMmcHostDriver<'d> {
                 0
             },
             __bindgen_anon_1: sdmmc_slot_config_t__bindgen_ty_1 {
-                cd: cd
-                    .map(|cd| cd.into_ref().deref().pin())
-                    .unwrap_or(SDMMC_SLOT_NO_CD),
+                cd: cd.map(|cd| cd.pin() as _).unwrap_or(SDMMC_SLOT_NO_CD),
             },
             __bindgen_anon_2: sdmmc_slot_config_t__bindgen_ty_2 {
-                wp: wp
-                    .map(|wp| wp.into_ref().deref().pin())
-                    .unwrap_or(SDMMC_SLOT_NO_WP),
+                wp: wp.map(|wp| wp.pin() as _).unwrap_or(SDMMC_SLOT_NO_WP),
             },
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
@@ -384,7 +378,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            clk: _clk.into_ref().deref().pin(),
+            clk: _clk.pin() as _,
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -397,7 +391,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            cmd: _cmd.into_ref().deref().pin(),
+            cmd: _cmd.pin() as _,
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -410,7 +404,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d0: _d0.into_ref().deref().pin(),
+            d0: _d0.pin() as _,
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -423,7 +417,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d1: _d1.map(|d1| d1.into_ref().deref().pin()).unwrap_or(-1),
+            d1: _d1.map(|d1| d1.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -436,7 +430,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d2: _d2.map(|d2| d2.into_ref().deref().pin()).unwrap_or(-1),
+            d2: _d2.map(|d2| d2.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -449,7 +443,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d3: _d3.map(|d3| d3.into_ref().deref().pin()).unwrap_or(-1),
+            d3: _d3.map(|d3| d3.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -462,7 +456,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d4: _d4.map(|d4| d4.into_ref().deref().pin()).unwrap_or(-1),
+            d4: _d4.map(|d4| d4.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -475,7 +469,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d5: _d5.map(|d5| d5.into_ref().deref().pin()).unwrap_or(-1),
+            d5: _d5.map(|d5| d5.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -488,7 +482,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d6: _d6.map(|d6| d6.into_ref().deref().pin()).unwrap_or(-1),
+            d6: _d6.map(|d6| d6.pin() as _).unwrap_or(-1),
             #[cfg(any(
                 esp_idf_soc_sdmmc_use_gpio_matrix,
                 not(any(
@@ -501,7 +495,7 @@ impl<'d> SdMmcHostDriver<'d> {
                     esp_idf_version_full = "5.3.2"
                 ))
             ))]
-            d7: _d7.map(|d7| d7.into_ref().deref().pin()).unwrap_or(-1),
+            d7: _d7.map(|d7| d7.pin() as _).unwrap_or(-1),
         };
 
         {
@@ -546,7 +540,7 @@ macro_rules! impl_slot {
     ($instance:ident: $slot:expr) => {
         crate::impl_peripheral!($instance);
 
-        impl SdMmc for $instance {
+        impl SdMmc for $instance<'_> {
             fn slot() -> u8 {
                 $slot
             }
